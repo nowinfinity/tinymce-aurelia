662a0be5114194dda945df2ea6f85af7998d8594
imressed <imressed@gmail.com>
2016-04-29 10:20:12 +0200

    update to new version

--- tinymce.js
+++ tinymce.js
@@ -4503,7 +4503,7 @@
             waitForWebKitLinkLoaded();
           }
           appendToHead(link);
-          link.href = 'jspm_packages/npm/tinymce@4.3.10/skins/lightgray/skin.min.css';
+          link.href = 'jspm_packages/github/imressed/tinymce-aurelia@master/skins/lightgray/skin.min.css';
         }
         this.load = load;
       };
@@ -22428,7 +22428,7 @@
               if (themeUrl) {
                 themeUrl = self.documentBaseURI.toAbsolute(themeUrl);
               } else {
-                themeUrl = 'jspm_packages/npm/tinymce@4.3.10/themes/' + settings.theme + '/theme' + suffix + '.js';
+                themeUrl = 'jspm_packages/github/imressed/tinymce-aurelia@master/themes/' + settings.theme + '/theme' + suffix + '.js';
               }
               ThemeManager.load(settings.theme, themeUrl);
             }
@@ -22659,7 +22659,7 @@
           }
           if (!skipWrite && !settings.content_editable) {
             doc.open();
-            doc.write('jspm_packages/npm/tinymce@4.3.10/skins/lightgray/content.min.css');
+            doc.write('jspm_packages/github/imressed/tinymce-aurelia@master/skins/lightgray/content.min.css');
             doc.close();
           }
           if (settings.content_editable) {
@@ -27762,4 +27762,4 @@
     });
     expose(["tinymce/geom/Rect", "tinymce/util/Promise", "tinymce/util/Delay", "tinymce/dom/EventUtils", "tinymce/dom/Sizzle", "tinymce/Env", "tinymce/util/Tools", "tinymce/dom/DomQuery", "tinymce/html/Styles", "tinymce/dom/TreeWalker", "tinymce/html/Entities", "tinymce/dom/DOMUtils", "tinymce/dom/ScriptLoader", "tinymce/AddOnManager", "tinymce/dom/RangeUtils", "tinymce/html/Node", "tinymce/html/Schema", "tinymce/html/SaxParser", "tinymce/html/DomParser", "tinymce/html/Writer", "tinymce/html/Serializer", "tinymce/dom/Serializer", "tinymce/util/VK", "tinymce/dom/ControlSelection", "tinymce/dom/BookmarkManager", "tinymce/dom/Selection", "tinymce/Formatter", "tinymce/UndoManager", "tinymce/EditorCommands", "tinymce/util/URI", "tinymce/util/Class", "tinymce/util/EventDispatcher", "tinymce/util/Observable", "tinymce/ui/Selector", "tinymce/ui/Collection", "tinymce/ui/ReflowQueue", "tinymce/ui/Control", "tinymce/ui/Factory", "tinymce/ui/KeyboardNavigation", "tinymce/ui/Container", "tinymce/ui/DragHelper", "tinymce/ui/Scrollable", "tinymce/ui/Panel", "tinymce/ui/Movable", "tinymce/ui/Resizable", "tinymce/ui/FloatPanel", "tinymce/ui/Window", "tinymce/ui/MessageBox", "tinymce/WindowManager", "tinymce/ui/Tooltip", "tinymce/ui/Widget", "tinymce/ui/Progress", "tinymce/ui/Notification", "tinymce/NotificationManager", "tinymce/EditorObservable", "tinymce/Shortcuts", "tinymce/Editor", "tinymce/util/I18n", "tinymce/FocusManager", "tinymce/EditorManager", "tinymce/util/XHR", "tinymce/util/JSON", "tinymce/util/JSONRequest", "tinymce/util/JSONP", "tinymce/util/LocalStorage", "tinymce/Compat", "tinymce/ui/Layout", "tinymce/ui/AbsoluteLayout", "tinymce/ui/Button", "tinymce/ui/ButtonGroup", "tinymce/ui/Checkbox", "tinymce/ui/ComboBox", "tinymce/ui/ColorBox", "tinymce/ui/PanelButton", "tinymce/ui/ColorButton", "tinymce/util/Color", "tinymce/ui/ColorPicker", "tinymce/ui/Path", "tinymce/ui/ElementPath", "tinymce/ui/FormItem", "tinymce/ui/Form", "tinymce/ui/FieldSet", "tinymce/ui/FilePicker", "tinymce/ui/FitLayout", "tinymce/ui/FlexLayout", "tinymce/ui/FlowLayout", "tinymce/ui/FormatControls", "tinymce/ui/GridLayout", "tinymce/ui/Iframe", "tinymce/ui/InfoBox", "tinymce/ui/Label", "tinymce/ui/Toolbar", "tinymce/ui/MenuBar", "tinymce/ui/MenuButton", "tinymce/ui/MenuItem", "tinymce/ui/Throbber", "tinymce/ui/Menu", "tinymce/ui/ListBox", "tinymce/ui/Radio", "tinymce/ui/ResizeHandle", "tinymce/ui/SelectBox", "tinymce/ui/Slider", "tinymce/ui/Spacer", "tinymce/ui/SplitButton", "tinymce/ui/StackLayout", "tinymce/ui/TabPanel", "tinymce/ui/TextBox"]);
   })(this);
-})(require('process'));
+})();
